aa74b89a0797a85c46aee82f260013e1db87fd00
Leo Van Rensburg <leo.van.rensburg@gmail.com>
2025-11-20 18:01:48 -0500

    upload working springboot code to git

--- src/main/java/org/big5/shop/ShopApplication.java
+++ src/main/java/org/big5/shop/ShopApplication.java
@@ -4,13 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.big5.shop.model.Database;
+
+import java.util.List;
 
 @RestController
 @SpringBootApplication
 public class ShopApplication {
     @RequestMapping("/")
 	String home() {
-		return "Hello World!";
+		String rtn = "";
+		List<Database.Item> items = Database.getAvailableItems();
+		for (Database.Item item: items) {
+			rtn = rtn + item.name + " ";
+		}
+		return rtn;
 	}
 
 	public static void main(String[] args) {
